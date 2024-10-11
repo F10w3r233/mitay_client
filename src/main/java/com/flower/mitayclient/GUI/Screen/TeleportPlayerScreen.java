65450d7594151e37d17850da47035c6be7ad01b0
13anx!aF10w3r <2986199164@qq.com>
2024-10-11 16:43:37 +0800

    Teleport Player Screen
    纵向按钮大于6个自动转向下一列

--- src/main/java/com/flower/mitayclient/GUI/Screen/TeleportPlayerScreen.java
+++ src/main/java/com/flower/mitayclient/GUI/Screen/TeleportPlayerScreen.java
@@ -29,8 +29,7 @@ public class TeleportPlayerScreen extends Screen
 {
 
     private static final Identifier MATRIX_LARGE = new ModIdentifier("textures/gui/sprites/screen/matrix_large.png");
-    private static final Identifier MATRIX_SIDE = new ModIdentifier("textures/gui/sprites/screen/matrix_side.png");
-    private static final Identifier INFO_BAR = new ModIdentifier("textures/gui/sprites/hud/info_bar_2x.png");
+
 
     MinecraftClient client = MinecraftClient.getInstance();
     public TeleportPlayerScreen()
@@ -38,12 +37,6 @@ public class TeleportPlayerScreen extends Screen
         super(Text.literal("Teleport_Player"));
     }
 
-
-    TeleportPlayerButton button = TeleportPlayerButton.builder(Text.literal(""), button ->
-    {
-        PlaceListScreen.sendChatCommand("tpw " + MinecraftClient.getInstance().getNetworkHandler().getPlayerListEntry("BanxiaFlower").getProfile().getName());
-    }).player(MinecraftClient.getInstance().getNetworkHandler().getPlayerListEntry("BanxiaFlower"))
-            .dimensions((this.client.getWindow().getScaledWidth()-382)/2+10,(this.client.getWindow().getScaledHeight()-292)/2+10, 210, 30).build();
 
 
     //按钮集合
@@ -53,8 +46,6 @@ public class TeleportPlayerScreen extends Screen
 
 
 
-
-    boolean flag;
 
 
     int originalPlayerNumber;
@@ -109,6 +100,10 @@ public class TeleportPlayerScreen extends Screen
                 }
                 if(curIndex == players.size())
                     init();
+                if(curIndex > 6)
+                {
+                    initX = (scaledWidth-382)/2+10 + 210 + 10;
+                }else initX = (scaledWidth-382)/2+10;
             }
         }
 
@@ -118,45 +113,8 @@ public class TeleportPlayerScreen extends Screen
 
 
 
-//        context.drawTexture(INFO_BAR, (scaledWidth-382)/2+10,(scaledHeight-292)/2+10,0,0,160,34,160,34);
-
-//        context.drawTexture(INFO_BAR, (scaledWidth-382)/2+10,(scaledHeight-292)/2+34+15,0,0,160,34,160,34);
-//        context.drawTexture(INFO_BAR, (scaledWidth-382)/2+160+22,(scaledHeight-292)/2+10,0,0,160,34,160,34);
 
 
-
-
-
-
-
-        int index = 0;
-        int curY;
-
-//        if(players != null)
-//        {
-//            for(PlayerListEntry player : players)
-//            {
-//                index++;
-//                curY = oriY + 20 * (index-1) + gap * (index-1);
-//
-//                if(player.getDisplayName() == null)
-//                {
-//                    if(player.getProfile().getName() != null)
-//                    {
-//                        context.drawTextWithShadow(client.textRenderer, player.getProfile().getName(), (this.client.getWindow().getScaledWidth()-382)/2+40, curY, 14474460);
-//                        if(player.getSkinTextures() != null)
-//                            PlayerSkinDrawer.draw(context, player.getSkinTextures(), (this.client.getWindow().getScaledWidth()-382)/2+20, curY-5, 16);
-////                        context.drawTextWithShadow(client.textRenderer, String.valueOf(player.getLatency()), 800, curY, 14474460);
-//
-//                    }
-//                }else
-//                {
-//                    if(player.getSkinTextures() != null)
-//                        PlayerSkinDrawer.draw(context, player.getSkinTextures(), (this.client.getWindow().getScaledWidth()-382)/2+20, curY-5, 16);
-//                    context.drawTextWithShadow(client.textRenderer, player.getDisplayName(), (this.client.getWindow().getScaledWidth()-382)/2+40, curY, 14474460);
-//                }
-//            }
-//        }
 
 
 
